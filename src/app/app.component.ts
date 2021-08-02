03433d0a4463878d210ca7d2ec270e980570cc14
suyogdeshmukh90 <suyogdeshmukh90@gmail.com>
2021-08-02 19:27:41 +0530

    todo final project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {TodoService} from "./todo.service";
+import {Todo} from "./todo";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-project';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import { TodoDetailsComponent } from './todo-details/todo-details.component';
+import { CreateTodoComponent } from './create-todo/create-todo.component';
+import { UpdateTodoComponent } from './update-todo/update-todo.component';
+import { SearchTodoComponent } from './search-todo/search-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoDetailsComponent,
+    CreateTodoComponent,
+    UpdateTodoComponent,
+    SearchTodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
